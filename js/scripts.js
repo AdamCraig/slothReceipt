fa5cc6afd0582b8c71eeb346d6841335dad5ca5f
adam craig and nick arrasate <adam@>
2016-03-23 10:59:57 -0700

    add initial commit for receipt using forms and JS to sell a sloth.

--- js/scripts.js
+++ js/scripts.js
@@ -1,97 +1,13 @@
-
-/*===Calculator Functions
-====================================*/
-
-var add = function(number1, number2) {
-	return number1 + number2;
-};
-
-var subtract = function(number1, number2) {
-	return number1 - number2;
-};
-
-var multiply = function(number1, number2) {
-	return number1 * number2;
-};
-
-var divide = function(number1, number2) {
-	return number1 / number2;
-};
-
 $(document).ready(function() {
 
- $("form#add").submit(function(event){
-	var number1 = parseInt($("#add1").val());
-	var number2 = parseInt($("#add2").val());
-	var result = add(number1, number2);
-	$("#addoutput").text(result);
+ $("form#info").submit(function(event){
+	var name = $("#name").val();
+	var address = $("#address").val();
+	$("#receiptname").text(name);
+	$("#receiptaddress").text(address);
+	$(".receipt").show();
 
 	event.preventDefault();
 
 	});
-
-	$("form#subtract").submit(function(event){
- 	var number1 = parseInt($("#sub1").val());
- 	var number2 = parseInt($("#sub2").val());
- 	var result = subtract(number1, number2);
- 	$("#suboutput").text(result);
-
- 	event.preventDefault();
-
- 	});
-
-	$("form#multiply").submit(function(event){
- 	var number1 = parseInt($("#mult1").val());
- 	var number2 = parseInt($("#mult2").val());
- 	var result = multiply(number1, number2);
- 	$("#multoutput").text(result);
-
- 	event.preventDefault();
-
- 	});
-
-	$("form#divide").submit(function(event){
- 	var number1 = parseInt($("#div1").val());
- 	var number2 = parseInt($("#div2").val());
- 	var result = divide(number1, number2);
- 	$("#divoutput").text(result);
-
- 	event.preventDefault();
-
- 	});
 });
-
-/*===BMI Functions
-====================================*/
-
-// var userBmi = function(weight,height) {
-// 	return (weight * 703) / (height * height);
-// };
-//
-// var weight = parseInt(prompt("How much do you weight?"));
-//
-// var height = parseInt(prompt("How tall are you?"));
-//
-// var result = userBmi(height,weight);
-//
-// // alert("Your BMI is " + result.toPrecision(4) + ".");
-
-/*===Temperature Functions
-====================================*/
-
-// var temp = parseInt(prompt("Enter Fahrenheit to convert to Celcius"));
-//
-// var fToC = function(temp) {
-// 	return temp * (9/5) + 32
-// };
-//
-// var cToF = function(temp) {
-// 	return (temp - 32) * (5/9);
-// };
-//
-// var result = cToF(temp);
-//
-// // alert(result.toPrecision(3));
-//
-// /*===Unit conversion Functions
-// ====================================*/
